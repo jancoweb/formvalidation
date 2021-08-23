b7275e8096eb1d437fb4e0a05dccee3624cd8aa1
João Coelho <jancoweb@outlook.com>
2021-08-23 17:33:32 -0300

    length check added

--- script.js
+++ script.js
@@ -33,10 +33,21 @@ function checkRequired(inputArr){
     }
   });
 }
+function checkLength(input, min, max){
+  if(input.value.length < min){
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if(input.value.length > max) {
+      showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    } else {
+      showSuccess(input)
+    }
+  }
 
 // Event Listeners
 form.addEventListener('submit', function(e){
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
   
   e.preventDefault();
 })
