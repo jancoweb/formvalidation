7b9a14b31c176b454a55107054ca45f26f865d40
João Coelho <jancoweb@outlook.com>
2021-08-23 17:12:52 -0300

    Aperfeicoamento de codigo

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function showError(input, message){
   formControl.className = 'form-control error';
   const small = formControl.querySelector('small');
   small.innerText = message;
-
 }
 function showSuccess(input){
   const formControl = input.parentElement;
@@ -21,34 +20,23 @@ function isValidEmail(email){
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+// Uppercase first char
+function getFieldName(input){
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+function checkRequired(inputArr){
+  inputArr.forEach(function(input){
+    if(input.value.trim() === ''){
+      showError(input, `${getFieldName(input)} is required`)
+    } else {
+      showSuccess(input);
+    }
+  });
+}
 
 // Event Listeners
 form.addEventListener('submit', function(e){
-  if(username.value === ''){
-    showError(username,'Username is required')
-  } else {
-    showSuccess(username);
-  }
-
-  if(email.value === ''){
-    showError(email,'Email is required');
-  } else if(!isValidEmail(email.value)) {
-    showError(email, "Email is not valid")
-  }else {
-    showSuccess(email);
-  }
-
-  if(password.value === ''){
-    showError(password,'Password is required')
-  } else {
-    showSuccess(password);
-  }
-
-  if(password2.value === ''){
-    showError(password2,'Password confirmation is required')
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
   
   e.preventDefault();
 })
